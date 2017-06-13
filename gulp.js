5b9bac218323d196878f517e23b13aa7025af748
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-06-14 00:16:55 +0530

    Installed gulp an gutil

---
+++ gulp.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util');//.test(aSome)
+
+gulp.tasks("log",function(){
+	gutil.log("workflows are awesome");
+});
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util');//.test(aSome)
+
+gulp.task("log",function(){
+	gutil.log("workflows are awesome");
+});
