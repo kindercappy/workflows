28237e3abbbec4ee2d334798c6aa16f638efc3a1
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-06-14 18:06:32 +0530

    Added Enviroment variable

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,36 @@ var gulp = require('gulp'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
 	connect = require('gulp-connect');
+var env,
+	coffeeSources,
+	jsonSources,
+	htmlSources,
+	sassSources,
+	jsonSources,
+	outputDir,
+	sassStyle;
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = [
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+}else {
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
-var htmlSources = ['builds/development/*.html'];
-var sassSources = ['components/sass/style.scss']
-var jsonSources = ['builds/development/js/*.json'];
+htmlSources = [outputDir + '*.html'];
+sassSources = ['components/sass/style.scss']
+jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task("coffee",function(){
 	gulp.src(coffeeSources)
@@ -28,7 +47,7 @@ gulp.task('js',function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js/'))
+		.pipe(gulp.dest(outputDir + 'js/'))
 		.pipe(connect.reload())
 	});
 
@@ -36,17 +55,17 @@ gulp.task('compass',function(){
 	gulp.src(sassSources)
 		.pipe(compass({
 			sass:'components/sass/',
-			image: 'builds/development/images/',
-			style: 'expanded'
+			image: outputDir + 'images/',
+			style: sassStyle
 		})
 		.on('error',gutil.log))
-		.pipe(gulp.dest('builds/development/css/'))
+		.pipe(gulp.dest(outputDir + 'css/'))
 		.pipe(connect.reload())
 });
 
 gulp.task('connect', function(){
 	connect.server({
-		root: 'builds/development/',
+		root: outputDir,
 		livereload: true
 	});
 });
